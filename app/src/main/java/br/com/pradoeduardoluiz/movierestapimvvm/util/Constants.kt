98e81b89688331935eb94676edb2349b95560a12
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2020-01-26 18:59:52 -0300

    Config REST API URL and KEY

---
+++ app/src/main/java/br/com/pradoeduardoluiz/movierestapimvvm/util/Constants.kt
@@ -0,0 +1,12 @@
+package br.com.pradoeduardoluiz.movierestapimvvm.util
+
+import br.com.pradoeduardoluiz.movierestapimvvm.BuildConfig
+
+object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3"
+
+    // YOU NEED YOUR OWN API KEY put in local.properties
+    const val API_KEY = BuildConfig.API_KEY
+
+}
